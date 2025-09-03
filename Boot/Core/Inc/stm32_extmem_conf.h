e1ea5c5897bf2983f9f95e9a2e13ed84859dde6b
Chris Nicolls <cnicolls@inmusicbrands.com>
2025-09-03 17:02:30 -0400

    Setting up XSPI1

--- Boot/Core/Inc/stm32_extmem_conf.h
+++ Boot/Core/Inc/stm32_extmem_conf.h
@@ -33,7 +33,7 @@
 */
 
 #define EXTMEM_DRIVER_NOR_SFDP   1
-#define EXTMEM_DRIVER_PSRAM      0
+#define EXTMEM_DRIVER_PSRAM      1
 #define EXTMEM_DRIVER_SDCARD     0
 #define EXTMEM_DRIVER_USER       0
 
@@ -53,6 +53,7 @@
 /* USER CODE END INCLUDE */
 /* Private variables ---------------------------------------------------------*/
 extern XSPI_HandleTypeDef hxspi2;
+extern XSPI_HandleTypeDef hxspi1;
 
 /* USER CODE BEGIN PV */
 
@@ -63,11 +64,13 @@ extern XSPI_HandleTypeDef hxspi2;
   * @{
   */
 enum {
-  EXTMEMORY_1  = 0 /*!< ID=0 for the first memory  */
+  EXTMEMORY_1  = 0, /*!< ID=0 for the first external memory  */
+  EXTMEMORY_2  = 1, /*!< ID=1 for the second external memory */
 };
 
 /* USER CODE BEGIN EC */
 #define EXT_MEMORY_NOR_FLASH  EXTMEMORY_1
+#define EXT_MEMORY_PSRAM      EXTMEMORY_2
 /* USER CODE END EC */
 
 /* Exported configuration --------------------------------------------------------*/
@@ -75,9 +78,9 @@ enum {
   * @{
   */
 
-extern EXTMEM_DefinitionTypeDef extmem_list_config[1];
+extern EXTMEM_DefinitionTypeDef extmem_list_config[2];
 #if defined(EXTMEM_C)
-EXTMEM_DefinitionTypeDef extmem_list_config[1];
+EXTMEM_DefinitionTypeDef extmem_list_config[2];
 #endif /* EXTMEM_C */
 
 /**
--- Boot/Core/Src/main.c
+++ Boot/Core/Src/main.c
@@ -45,6 +45,7 @@
 
 UART_HandleTypeDef huart4;
 
+XSPI_HandleTypeDef hxspi1;
 XSPI_HandleTypeDef hxspi2;
 
 /* USER CODE BEGIN PV */
@@ -58,6 +59,7 @@ static void MX_GPIO_Init(void);
 static void MX_UART4_Init(void);
 static void MX_SBS_Init(void);
 static void MX_XSPI2_Init(void);
+static void MX_XSPI1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -109,6 +111,7 @@ int main(void)
   MX_UART4_Init();
   MX_SBS_Init();
   MX_XSPI2_Init();
+  MX_XSPI1_Init();
   MX_EXTMEM_MANAGER_Init();
   /* USER CODE BEGIN 2 */
 
@@ -271,6 +274,56 @@ static void MX_UART4_Init(void)
 }
 
 /**
+  * @brief XSPI1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_XSPI1_Init(void)
+{
+
+  /* USER CODE BEGIN XSPI1_Init 0 */
+
+  /* USER CODE END XSPI1_Init 0 */
+
+  XSPIM_CfgTypeDef sXspiManagerCfg = {0};
+
+  /* USER CODE BEGIN XSPI1_Init 1 */
+
+  /* USER CODE END XSPI1_Init 1 */
+  /* XSPI1 parameter configuration*/
+  hxspi1.Instance = XSPI1;
+  hxspi1.Init.FifoThresholdByte = 4;
+  hxspi1.Init.MemoryMode = HAL_XSPI_SINGLE_MEM;
+  hxspi1.Init.MemoryType = HAL_XSPI_MEMTYPE_APMEM_16BITS;
+  hxspi1.Init.MemorySize = HAL_XSPI_SIZE_256MB;
+  hxspi1.Init.ChipSelectHighTimeCycle = 5;
+  hxspi1.Init.FreeRunningClock = HAL_XSPI_FREERUNCLK_DISABLE;
+  hxspi1.Init.ClockMode = HAL_XSPI_CLOCK_MODE_0;
+  hxspi1.Init.WrapSize = HAL_XSPI_WRAP_NOT_SUPPORTED;
+  hxspi1.Init.ClockPrescaler = 0;
+  hxspi1.Init.SampleShifting = HAL_XSPI_SAMPLE_SHIFT_NONE;
+  hxspi1.Init.DelayHoldQuarterCycle = HAL_XSPI_DHQC_DISABLE;
+  hxspi1.Init.ChipSelectBoundary = HAL_XSPI_BONDARYOF_8KB;
+  hxspi1.Init.MaxTran = 0;
+  hxspi1.Init.Refresh = 0;
+  hxspi1.Init.MemorySelect = HAL_XSPI_CSSEL_NCS1;
+  if (HAL_XSPI_Init(&hxspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sXspiManagerCfg.nCSOverride = HAL_XSPI_CSSEL_OVR_NCS1;
+  sXspiManagerCfg.IOPort = HAL_XSPIM_IOPORT_1;
+  if (HAL_XSPIM_Config(&hxspi1, &sXspiManagerCfg, HAL_XSPI_TIMEOUT_DEFAULT_VALUE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN XSPI1_Init 2 */
+
+  /* USER CODE END XSPI1_Init 2 */
+
+}
+
+/**
   * @brief XSPI2 Initialization Function
   * @param None
   * @retval None
@@ -337,6 +390,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPION_CLK_ENABLE();
   __HAL_RCC_GPIOO_CLK_ENABLE();
+  __HAL_RCC_GPIOP_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOM, LED4_Pin|LED3_Pin, GPIO_PIN_SET);
